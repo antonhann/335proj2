d07e0ab5aa50e0464892841ae29c68b628f28ca9
antonhann <antonha016@gmail.com>
2024-04-05 22:16:52 -0400

    fixed heap

--- myHeap.cpp
+++ myHeap.cpp
@@ -60,9 +60,42 @@
 //         }
 //     } 
 // }
-
-// void heapMedian (const std::vector<int> * instructions){
-
+#include <cmath>
+#include <list>
+#include<vector>
+void heapMedian (const std::vector<int> * instructions){
+std::list<int> myList;
+    std::vector<int> res;
+    std::list<int>::iterator listIt;
+    bool inserted;
+    for(auto it = instructions->begin(); it != instructions->end(); ++it){
+        inserted = false;
+        if(*it != -1){
+            if(myList.empty()){
+                myList.push_front(*it);
+            }else{
+                for(listIt = myList.begin(); listIt != myList.end(); ++listIt){
+                    if(*listIt >= *it){
+                        myList.insert(listIt, *it);
+                        inserted = true;
+                        break;
+                    }
+                }
+                if(!inserted){
+                    myList.push_back(*it);
+                }
+            }
+        }else{
+            listIt = myList.begin();
+            std::advance(listIt, ceil(myList.size() / 2.0) - 1);
+            res.push_back(*listIt);
+            myList.erase(listIt);
+        }
+    }
+    for(int i = 0; i < res.size(); i++){
+        std::cout << res[i] << " ";
+    } 
+}
 //     std::priority_queue<int> large;
 //     std::priority_queue<int, std::vector<int>, std::greater<int>> small;
 //     std::vector<int> res;
