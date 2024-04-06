d2121eb07dcb992ca1dad19574ff7d77291a5c50
antonhann <antonha016@gmail.com>
2024-04-05 22:15:07 -0400

    fixed heap

--- myHeap.hpp
+++ myHeap.hpp
@@ -1,9 +1,9 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <vector>
-#include <climits>
-#include <cmath>
-#include <queue>
-void heapMedian (const std::vector<int> * instructions);
-void balanceHeaps(std::priority_queue<int>& large,std::priority_queue<int, std::vector<int>, std::greater<int>>& small, int val);
+// #include <iostream>
+// #include <fstream>
+// #include <cstdlib>
+// #include <vector>
+// #include <climits>
+// #include <cmath>
+// #include <queue>
+// void heapMedian (const std::vector<int> * instructions);
+// void balanceHeaps(std::priority_queue<int>& large,std::priority_queue<int, std::vector<int>, std::greater<int>>& small, int val);
