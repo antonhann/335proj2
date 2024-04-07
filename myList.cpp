45fb3bb530f4965a423c4c23bba82c5904261359
antonhann <antonha016@gmail.com>
2024-04-07 17:40:07 -0400

    hello

--- myList.cpp
+++ myList.cpp
@@ -3,6 +3,7 @@ void listMedian (const std::vector<int> * instructions){
     std::list<int> myList;
     std::vector<int> res;
     std::list<int>::iterator listIt;
+    bool inserted;
     const auto start = std::chrono::steady_clock::now();
     for(auto it = instructions->begin(); it != instructions->end(); ++it){
         inserted = false;
@@ -13,10 +14,11 @@ void listMedian (const std::vector<int> * instructions){
                 for(listIt = myList.begin(); listIt != myList.end(); ++listIt){
                     if(*listIt >= *it){
                         myList.insert(listIt, *it);
+                        inserted = true;
                         break;
                     }
                 }
-                if(listIt == myList.end()){
+                if(!inserted){
                     myList.push_back(*it);
                 }
             }
